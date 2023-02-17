a7e88dc4fe193cddba25a8cd00f16a59ba88ecde
tohieungoan <ngoanth.22it@vku.udn.vn>
2023-02-17 22:08:29 +0700

    upload

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const menu = document.querySelector(".navbar__links")
+const menuButton = document.querySelector(".navbar__icons")
+
+menuButton.addEventListener('click', () =>{
+    menu.classList.toggle("navbar__open");
+    menuButton.classList.toggle("open");
+});
+
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 5000);
+}
+
